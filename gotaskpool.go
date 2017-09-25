389ea51c525d51fc8191ce7d61a04bab0241d21c
fangyousong <fangyousong@qq.com>
2017-09-25 14:32:46 +0800

    +prior task pool

--- gotaskpool.go
+++ gotaskpool.go
@@ -10,9 +10,9 @@ type GoTask interface {
 }
 
 type LimitedTaskQueue struct {
-	queue []GoTask
-	sem chan int
-	size int32
+	queue      []GoTask
+	sem        chan int
+	size       int
 	producePos int
 	consumePos int
 	sync.Mutex
@@ -21,20 +21,24 @@ type LimitedTaskQueue struct {
 func NewLimitedTaskQueue(limit int) *LimitedTaskQueue {
 	ret := new(LimitedTaskQueue)
 	ret.size = 0
-	ret.queue = make([]GoTask,limit)
+	ret.queue = make([]GoTask, limit)
 	ret.sem = make(chan int, limit)
 	ret.producePos = -1
 	ret.consumePos = -1
 	return ret
 }
 
+func (m *LimitedTaskQueue) GetSize() int {
+	return m.size
+}
+
 func (m *LimitedTaskQueue) Push(task GoTask, waitDuration time.Duration) bool {
-	if task == nil  {
+	if task == nil {
 		return false
 	}
 	if waitDuration > 0 {
 		select {
-		case <- time.After(waitDuration):
+		case <-time.After(waitDuration):
 			return false
 		case m.sem <- 1:
 		}
@@ -56,12 +60,12 @@ func (m *LimitedTaskQueue) Push(task GoTask, waitDuration time.Duration) bool {
 func (m *LimitedTaskQueue) Pull(waitDuration time.Duration) GoTask {
 	if waitDuration >= 0 {
 		select {
-		case <- time.After(waitDuration):
+		case <-time.After(waitDuration):
 			return nil
-		case <- m.sem:
+		case <-m.sem:
 		}
 	} else {
-		<- m.sem
+		<-m.sem
 	}
 	m.Lock()
 	defer m.Unlock()
@@ -73,14 +77,17 @@ func (m *LimitedTaskQueue) Pull(waitDuration time.Duration) GoTask {
 	return m.queue[m.consumePos]
 }
 
-type TaskPool struct{
-	taskQueue *LimitedTaskQueue
+type TaskPool struct {
+	taskQueue      *LimitedTaskQueue
 	goroutineCount int
-	stopped bool
-	started bool
+	stopped        bool
+	started        bool
 }
 
-func NewTaskPool(poolSize int, workerCount int) *TaskPool{
+func NewTaskPool(poolSize int, workerCount int) *TaskPool {
+	if poolSize <= 0 || workerCount <= 0 {
+		return nil
+	}
 	ret := new(TaskPool)
 	ret.taskQueue = NewLimitedTaskQueue(poolSize)
 	ret.goroutineCount = workerCount
@@ -103,19 +110,100 @@ func (m *TaskPool) PullTask(waitDuration time.Duration) GoTask {
 
 func (m *TaskPool) Start() {
 	if m.started {
-		return
-	}
-	m.started = true
-	for i := 0; i < m.goroutineCount; i++ {
-		go func() {
-			for{
-				task := m.PullTask(-1)
-				task.Run()
-			}
-		}()
+		m.stopped = false
+	} else {
+		m.started = true
+		m.stopped = false
+		for i := 0; i < m.goroutineCount; i++ {
+			go func() {
+				for {
+					task := m.PullTask(-1)
+					task.Run()
+				}
+			}()
+		}
 	}
 }
 
 func (m *TaskPool) Stop() {
+	m.stopped = true
+}
+
+type PriorTaskPool struct {
+	sync.Mutex
+	taskQueues     []*LimitedTaskQueue
+	taskSem        chan int
+	goroutineCount int
+	stopped        bool
+	started        bool
+}
+
+func NewPriorTaskPool(priorityNum int, queueLen int, workerCount int) *PriorTaskPool {
+	if priorityNum <= 0 || queueLen <= 0 || workerCount <= 0 {
+		return nil
+	}
+	ret := new(PriorTaskPool)
+	ret.taskSem = make(chan int, priorityNum*queueLen)
+	ret.goroutineCount = workerCount
+	ret.stopped = false
+	ret.started = false
+	ret.taskQueues = make([]*LimitedTaskQueue, 0, priorityNum)
+	for i := 0; i < priorityNum; i++ {
+		queue := NewLimitedTaskQueue(queueLen)
+		ret.taskQueues = append(ret.taskQueues, queue)
+	}
+	return ret
+}
+
+func (m *PriorTaskPool) PushTask(priority int, task GoTask, waitDuration time.Duration) bool {
+	if m.stopped || priority >= len(m.taskQueues) || priority < 0 {
+		return false
+	}
+	ret := m.taskQueues[priority].Push(task, waitDuration)
+	if ret {
+		m.taskSem <- 1
+	}
+
+	return ret
+}
+
+func (m *PriorTaskPool) PullTask(waitDuration time.Duration) GoTask {
+	tm := waitDuration
+	if tm <= 0 {
+		tm = time.Hour * 30
+	}
+	select {
+	case <-m.taskSem:
+		m.Lock()
+		defer m.Unlock()
+		for i := 0; i < len(m.taskQueues); i++ {
+			if m.taskQueues[i].GetSize() > 0 {
+				return m.taskQueues[i].Pull(tm)
+			}
+		}
+	case <-time.After(tm):
+		return nil
+	}
+	return nil
+}
+
+func (m *PriorTaskPool) Start() {
+	if m.started {
+		m.stopped = false
+	} else {
+		m.started = true
+		m.stopped = false
+		for i := 0; i < m.goroutineCount; i++ {
+			go func() {
+				for {
+					task := m.PullTask(-1)
+					task.Run()
+				}
+			}()
+		}
+	}
+}
+
+func (m *PriorTaskPool) Stop() {
 	m.stopped = true
 }
--- main/gotasktooltest.go
+++ main/gotasktooltest.go
@@ -14,15 +14,55 @@ func (m *mytask) Run() {
 	fmt.Printf("%s, mytask is running.\n", time.Now().String())
 }
 
+type mypriortask struct {
+	priorty int
+}
+
+func (m *mypriortask) Run() {
+	fmt.Printf("%s, mytask[%d] is running.\n", time.Now().String(), m.priorty)	
+}
+
 func main() {
-	pool := gotaskpool.NewTaskPool(1024,10)
-	pool.Start()
-	for{
-		pool.PushTask(&mytask{}, -1)
-		//<-time.After(time.Second)	
+{
+		pool := gotaskpool.NewPriorTaskPool(5,1024,10)
+
+		pool.Start()
+		for {
+			pool.PushTask(0,&mypriortask{priorty: 0}, -1)
+			pool.PushTask(1,&mypriortask{priorty: 1}, -1)
+			pool.PushTask(2,&mypriortask{priorty: 2}, -1)
+			pool.PushTask(3,&mypriortask{priorty: 3}, -1)
+			pool.PushTask(4,&mypriortask{priorty: 4}, -1)
+
+			pool.PushTask(0,&mypriortask{priorty: 0}, -1)
+			pool.PushTask(1,&mypriortask{priorty: 1}, -1)
+			pool.PushTask(2,&mypriortask{priorty: 2}, -1)
+			pool.PushTask(3,&mypriortask{priorty: 3}, -1)
+			pool.PushTask(4,&mypriortask{priorty: 4}, -1)
+
+			pool.PushTask(0,&mypriortask{priorty: 0}, -1)
+			pool.PushTask(1,&mypriortask{priorty: 1}, -1)
+			pool.PushTask(2,&mypriortask{priorty: 2}, -1)
+			pool.PushTask(3,&mypriortask{priorty: 3}, -1)
+			pool.PushTask(4,&mypriortask{priorty: 4}, -1)
+			fmt.Println("===================")
+			<-time.After(time.Second)
+		}
+		return 
 	}
 
-	return
+	{
+		pool := gotaskpool.NewTaskPool(1024,10)
+		pool.Start()
+		for{
+			pool.PushTask(&mytask{}, -1)
+			//<-time.After(time.Second)	
+		}
+
+		return
+	}
+
+
 	
 
 }
